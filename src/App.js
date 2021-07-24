f70bbda6c51cd7cba2137b7940c8f9ce2622d5bb
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-07-23 22:19:57 -0400

    Number - 399

--- src/App.js
+++ src/App.js
@@ -3,12 +3,20 @@ import './App.css';
 const fname = "Wassay";
 const lname = "Bajwa";
 const luckyNumber = 7;
+const img = "https://picsum.photos/200";
+const customStyle = {
+  color: "white",
+  fontSize: "15px",
+  border: "1px solid"
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>This website is created by {fname} {lname}</h1>
-        <h5>Copyright {dateYear()}</h5>
+        <h1 className="heading">This website is created by {fname} {lname}</h1>
+        <h5 style={customStyle}>Copyright {dateYear()}</h5>
+        <img src={img + "?grayscale"} alt="images"></img>
+
         {/* <h1>
           Hello world i am here now {fname + " " + lname} */}
           {/* ES6 Template literals {`${fname} ${lname}`} */}
