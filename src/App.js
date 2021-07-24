5ea84e2a68e6436b59099ec6b044bb780945b7d4
Wassay Bajwa <71424282+wassaybajwa@users.noreply.github.com>
2021-07-23 21:59:11 -0400

    Number - 398

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-
+const fname = "Wassay";
+const lname = "Bajwa";
+const luckyNumber = 7;
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This website is created by {fname} {lname}</h1>
+        <h5>Copyright {dateYear()}</h5>
+        {/* <h1>
+          Hello world i am here now {fname + " " + lname} */}
+          {/* ES6 Template literals {`${fname} ${lname}`} */}
+          {/* {fname} {lname} */}
+        {/* </h1>
+        <h3>my lucky number is {Math.floor(Math.random() * 10)}</h3>
+        <ul>
+          <li>Grapes</li>
+          <li>Apples</li>
+          <li>Jamon</li>
+        </ul> */}
       </header>
     </div>
   );
 }
+
+ function dateYear() {
+  return (
+      <p>{new Date().getFullYear()}</p>
+  );
+}
+
+
 
 export default App;
